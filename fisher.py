15153ebf25a50f1becb5b68dc965eeadc7f6728b
tqtl911@163.com <19930911cXS>
2018-09-25 23:57:06 +0800

    Initial commit

---
+++ fisher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Project: FishBook 
+# Software: PyCharm
+# Time    : 2018-09-25 23:54
+# File    : fisher.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    return "Hello World！Base WebFramework Flask"
+
+
+if __name__ == '__main__':
+    app.run()
